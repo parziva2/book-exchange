d2e39228a54f94b45ed94959b064262842cdcac6
Parziva <alexfedco2@gmail.com>
2025-01-07 14:16:00 +0000

    Fix: Update MongoDB connection and client build configuration

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/sessions', sessionRoutes);
 app.use('/api/chat', chatRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
